6221e3a03e07173651f1e4feef98b7e8ece900f2
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2025-02-27 13:43:11 -0300

    Alterar configuração do TypeOrm para usar conexão assíncrona e aguardar 5 segundos antes de conectar ao banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,26 @@ import { User, Table, Order, Payment, MenuItem } from './modules';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST || 'localhost',
-      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
-      username: process.env.DATABASE_USER || 'postgres',
-      password: process.env.DATABASE_PASSWORD || 'password',
-      database: process.env.DATABASE_NAME || 'deuquantas',
-      autoLoadEntities: true,
-      synchronize: true, // ⚠️ Apenas para desenvolvimento, desativar em produção!
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        console.log('⏳ Aguardando o banco de dados...');
+        await new Promise((resolve) => setTimeout(resolve, 5000)); // Aguarda 5 segundos antes de conectar
+        console.log('✅ Conectando ao banco de dados!');
+        return {
+          type: 'postgres',
+          host: process.env.DATABASE_HOST || 'localhost',
+          port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+          username: process.env.DATABASE_USER || 'postgres',
+          password: process.env.DATABASE_PASSWORD || 'password',
+          database: process.env.DATABASE_NAME || 'deuquantas',
+          autoLoadEntities: true,
+          synchronize: true, // ⚠️ Apenas para desenvolvimento, desativar em produção!
+          logging: true,
+        };
+      },
     }),
     TypeOrmModule.forFeature([User, Table, Order, Payment, MenuItem]),
   ],
 })
 export class AppModule {}
+
