cd775b481f9c7256b48529c668aff808c172edcc
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2025-02-27 13:45:54 -0300

    Adicionar suporte ao Swagger para documentação da API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Configurar o Swagger
+  const config = new DocumentBuilder()
+    .setTitle('DeuQuantas API')
+    .setDescription('Documentação da API do DeuQuantas')
+    .setVersion('1.0')
+    .addBearerAuth() // Adiciona autenticação JWT se necessário
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Define o endpoint /api
+
+  await app.listen(3000);
 }
 bootstrap();
